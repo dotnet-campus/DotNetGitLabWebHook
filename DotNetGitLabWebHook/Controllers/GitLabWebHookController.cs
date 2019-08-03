9df2b5c9d8b2e290eb5d6b0ae838b82b2c3251f5
lindexi <lindexi_gd@163.com>
2019-08-03 14:23:28 +0800

    添加项目文件。

---
+++ DotNetGitLabWebHook/Controllers/GitLabWebHookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetGitLabWebHook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GitLabWebHookController : ControllerBase
+    {
+        [HttpPost]
+        [Route("MergeRequest")]
+        public IActionResult MergeRequest(object obj)
+        {
+            return Ok();
+        }
+    }
+}
